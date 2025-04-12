525cc38b7266083a4efeb1fcdf38c85878ee1631
Warren Snipes <contact@warrensnipes.dev>
2025-04-12 02:34:14 +0000

    feat(orm): :card_file_box: Add map table

--- backend/migration/src/lib.rs
+++ backend/migration/src/lib.rs
@@ -1,6 +1,7 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20250412_020153_init_tables;
+mod m20250412_022647_add_map_table;
 
 pub struct Migrator;
 
@@ -9,6 +10,7 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20250412_020153_init_tables::Migration),
+            Box::new(m20250412_022647_add_map_table::Migration),
         ]
     }
 }
